026d3297a77c712913975b5cf85ba33e14060ca9
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2021-02-23 23:56:13 +0100

    push-config.sh adding ..

---
+++ my/push-config.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME status         
+/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME add -u         
+/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME status         
+/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME commit -m "$1" 
+/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME push           
