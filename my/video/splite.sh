6eee4e5720a8d2ad1b06e0e2b0e11b1088e2496c
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2021-02-27 01:43:15 +0100

    add splite.sh +1

--- my/video/splite.sh
+++ my/video/splite.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 # var
-input="$1"
-output="$2"
-startVideo=$3
-endVideo=$4
+# input="$1"
+# output="$2"
+# startVideo=$3
+# endVideo=$4
 
 
 # (in,out,x,y,w,h)
 #ffmpeg -i $1 -filter:v "crop=$5:$6:$3:$4" -c:a copy $2 -y
 #ffmpeg -i $input -filter:v "crop=$w:$h:$x:$y" -c:a copy $output -y
-ffmpeg -i $input -ss $sratVideo -t $endVideo -async 1 -c:a copy $output -y
-
-
+# ffmpeg  -ss $sratVideo -i $input -c copy -t $endVideo  $output -y
+ffmpeg -ss 30 -i first.mp4 -c copy -t 10 output.mp4
+##
+# -i  input file
+# -ss start time in seconds or in hh:mm:ss
+# -t how time in seconds or in hh:mm:ss
+# -to end time in seconds or in hh:mm:ss
+# -c codec to use
+##
