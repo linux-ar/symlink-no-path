55120a58f4f0f72853f49df8515c69e9658beaf4
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2021-02-28 14:47:26 +0000

    add cut-by-vps.sh +2

--- my/video/cut-by-vps.sh
+++ my/video/cut-by-vps.sh
@@ -2,7 +2,7 @@
 # https://youtu.be/mQal3i1rPm4
 # var
 # url='https://youtu.be/mQal3i1rPm4'
-input="$HOME/vid/vps.mp4"
+input="$HOME/vid/vps.mp4.mkv"
 output="$HOME/vid/vps-output.mp4"
 
 url="$1"
@@ -10,6 +10,7 @@ startVideo="$2"
 endVideo="$3"
 
 sudo rm $HOME/vid/vps.mp4 >/dev/null 2>&1
+sudo rm $HOME/vid/vps.mp4.mkv >/dev/null 2>&1
 sudo rm $HOME/vid/vps-output.mp4 >/dev/null 2>&1
 
 youtube-dl -f 'bestvideo[height<=480]+bestaudio/best[height<=480]' -o '$HOME/vid/vps.mp4' $url
