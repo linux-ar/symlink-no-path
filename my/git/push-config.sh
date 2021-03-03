bd39bf4a97d944ecaaae57fb0610e22dac0ddce2
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2021-03-03 10:16:01 +0100

    reload alias after modifie any config

--- my/git/push-config.sh
+++ my/git/push-config.sh
@@ -5,3 +5,4 @@
 /usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME status         
 /usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME commit -m "$1" 
 /usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME push           
+source $HOME/.alias
