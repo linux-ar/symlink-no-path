c5f424a80c521d8c2cce2a93ed6309f91d3ccd54
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2021-03-10 21:59:28 +0100

    use 480p in termux download portion

--- my/video/portionFromYoutube.sh
+++ my/video/portionFromYoutube.sh
@@ -1,10 +1,9 @@
-#!/bin/bash
-url=$(youtube-dl --youtube-skip-dash-manifest -g $URLP)
+#!/data/data/com.termux/files/usr/bin/bash
+url=$(youtube-dl -f 'bestvideo[height<=480]+bestaudio/best[height<=480]' --youtube-skip-dash-manifest -g $URLP)
 video_url=$(echo -e $url |sed -n '1 p')
 audio_url=$(echo -e $url |sed -n '2 p')
 retard_time=0
-rand=$(openssl rand -base64 9 | cut -c -6)
-$rand=$(echo $rand | sed  's/[^a-zA-Z]//g')
+rand=$(openssl rand -base64 48 | sed 's/[^a-z]//g'|sed 's/^\(......\).*/\1/')
 # because input sometime is string 0:04:30
 # if [[ $STARTVIDEOP -gt 30 ]]; then
 #     $STARTVIDEOP=$(($STARTVIDEOP-30)) 
@@ -12,19 +11,26 @@ $rand=$(echo $rand | sed  's/[^a-zA-Z]//g')
 #     retard_time=30
 # fi
 
-ffmpeg                        \
-    -y                        \
-    -ss "$STARTVIDEOP"        \
-    -i  "$video_url"          \
-    -ss "$STARTVIDEOP"        \
-    -i  "$audio_url"          \
-    -map 0:v                  \
-    -map 1:a                  \
-    -ss "$retard_time"        \
-    -t "$ENDVIDEOP"           \
-    -c:v libx264              \
-    -c:a aac                  \
+ffmpeg                              \
+    -y                              \
+    -ss "$STARTVIDEOP"              \
+    -i  "$video_url"                \
+    -ss "$STARTVIDEOP"              \
+    -i  "$audio_url"                \
+    -map 0:v                        \
+    -map 1:a                        \
+    -ss "$retard_time"              \
+    -t "$ENDVIDEOP"                 \
+    -b: 500k                        \
+    -threads 8                      \
+    -crf 18                         \
+    -vsync vfr                      \
+    -preset ultrafast               \
     $HOME/storage/movies/vps-output-$rand.mp4
+
+    # -c copy                   \
+    # -c:v libx264              \
+    # -c:a aac                  \
 
 
 
@@ -39,3 +45,63 @@ ffmpeg                        \
  #    -t 1:10
  #    -c:v libx264
  #    -c:a aac gog-vs-triv.mkv
+ 
+# speed but not commress my fideo
+# ffmpeg -i $input  -ss $startVideo -t $endVideo -c copy  $output -y
+#backup worked
+# ffmpeg -ss $startVideo -i $input  -t $endVideo -c:a copy $output -y
+# work better and compress very sall
+# ffmpeg 
+#     \    -y
+#     \    -ss $startVideo 
+#     \    -i $input  
+#     \    -t $endVideo  
+#     \    -vcodec libx265 
+#     \    -crf 30 
+#     \    $output 
+
+# ffmpeg     \
+#     -y    \
+#     -hide_banner \
+#     -ss $startVideo     \
+#     -i $input      \
+#     -vcodec libx265     \
+#     -crf 30     \
+#     -b: 500k     \
+#     -threads 8 \
+#     $output     
+#     # -t $endVideo      \
+#     # -vsync vfr     \
+
+
+# ffmpeg                              \
+#     -y                              \
+#     -ss $startVideo                 \
+#     -i $input                       \
+#     -b: 500k                        \
+#     -threads 8                      \
+#     -vcodec libstagefright_h264     \
+#     -crf 26                         \
+#     -t $endVideo                    \
+#     $output     
+
+    # --disable-asm    \
+    # -vsync vfr     \
+    # -preset ultrafast     \
+
+# -t how time you want
+# -to what's time to end
+
+    # -vcodec libstagefright_h264     \
+    #
+    #
+    #
+# for remove acc licence
+# dont use this flag : -c:a copy
+# for fast video only use -c:a copy
+# audio must convert automaticaly
+# for fast video+audio :
+# it's accepte in format that not licended as:
+#   - youtube
+#   - facebook
+# youtube 480p format it's not supported by whatsapp
