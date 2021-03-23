88d1b5f1d3a35668f22cbc51121f3919120148e1
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2021-03-23 01:46:00 +0100

    cd to folder : adding exclude .git .cache ..

--- my/find/cd_to_dir.sh
+++ my/find/cd_to_dir.sh
@@ -1,24 +1,44 @@
 #!/bin/bash
 
-# fd - Find any directory and cd to selected directory
-cdd() {
- export FZF_DEFAULT_OPTS='--height=95% '
- 
- cd $(find . -maxdepth 4 -type d -print| fzf) && \
-   clear           && \
-   echo -e "\n\n"  && \
-   $PREFIX/bin/exa && \
-   echo -e "\n\n"
-
- export FZF_DEFAULT_OPTS='--height=70% --preview="cat {}" --preview-window=right:60%:wrap'
-}
+# cd to folder by name of files
 
 cdf() {
- 
- pushd $(dirname $(fzf)) && \
+ # pushd $(dirname $(fzf)) && \
+ pushd $(dirname $(fid --type f --hidden --follow --exclude ".git" --exclude "dotfiles")) && \
    clear                 && \
    echo -e "\n\n"        && \
    $PREFIX/bin/exa       && \
    echo -e "\n\n"
 }
 
+cdd() {
+ export FZF_DEFAULT_OPTS='--height=95% '
+ cd                                 \
+    fd --type d                     \
+       --hidden                     \
+       --follow                     \
+       --exclude "dotfiles"         \
+       --exclude ".git"             \
+       --exclude ".npm"             \
+       --exclude ".cache" . | fzf   \
+        &&\
+  $PREFIX/bin/exa && \
+  echo -e "\n"
+ export FZF_DEFAULT_OPTS='--height=70% --preview="cat {}" --preview-window=right:60%:wrap'
+}
+
+cff() {
+ export FZF_DEFAULT_OPTS='--height=95% '
+ pushd $(dirname $(fd --type f          \
+       --hidden                         \
+       --follow                         \
+       --exclude "dotfiles"             \
+       --exclude ".git"                 \
+       --exclude ".npm"                 \
+       --exclude ".cache" . | fzf ))
+  clear
+  $PREFIX/bin/exa 
+  printf "\033[1;4;35m%-${COLUMNS}s\033[00m"
+  echo " "
+ export FZF_DEFAULT_OPTS='--height=70% --preview="cat {}" --preview-window=right:60%:wrap'
+}
