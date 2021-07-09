5469687c419c8aa7c7955c87430876c34ddc0cba
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2021-07-09 20:10:17 +0100

    update

--- suckless/dwm-ayoub/ayoubStatusBar.sh
+++ suckless/dwm-ayoub/ayoubStatusBar.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-secondeBar=$2
-stopwatch(){
-  local var=$secondeBar
-  # [[ $1 =~ "chrono" ]] && let secondeBar=$secondeBar-1 || let secondeBar=""
-  if [ "$1" == "chrono" ] && [ ! -z "$2" ]; then
-    secondeBar=33
-  fi
-  return $var
-}
-fff(){
 while true;do
   date="$(date +%a\ %d\ %b\ %H:%M:%S)"
   ram="$(free -m | awk 'NR==2 { print $3 }')"
@@ -19,12 +9,7 @@ while true;do
   [[ $statusBatery =~ "Discharging" ]] && statusBaterBar="off"
   statusbarPrimary="${battrie}%$statusBaterBar | ${ram} MB | ${date}" 
   
-  secondeBar=$(stopwatch)
   statusbar="$secondeBar | $statusbarPrimary"
   exec xsetroot -name "$statusbar"&
   sleep 1
 done
-}
-stopwatch
-echo "$secondeBar"
-
