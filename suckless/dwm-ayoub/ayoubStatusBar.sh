23d0fbae6651df21907ff91346860a52e0b0ddeb
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2021-07-12 03:35:36 +0100

    add chrono var if not du -b /tmp/chrono==1 bayt

--- suckless/dwm-ayoub/ayoubStatusBar.sh
+++ suckless/dwm-ayoub/ayoubStatusBar.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 while true;do
+  bateryStatus="$(cat /sys/class/power_supply/axp288_fuel_gauge/status)"
+  [[ $bateryStatus =~ "Discharging" ]] && bateryFlag="off" || bateryFlag="on"
+  battrie="$(cat /sys/class/power_supply/axp288_fuel_gauge/capacity)% ${bateryFlag}"
+
   date="$(date +%a\ %d\ %b\ %H:%M:%S)"
-  ram="$(free -m | awk 'NR==2 { print $3 }')"
-  battrie="$(cat /sys/class/power_supply/axp288_fuel_gauge/capacity)"
-  statusBatery="$(cat /sys/class/power_supply/axp288_fuel_gauge/status)"
-  statusBaterBar="on"
-  [[ $statusBatery =~ "Discharging" ]] && statusBaterBar="off"
-  statusbarPrimary="${battrie}%$statusBaterBar | ${ram} MB | ${date}" 
-  
-  statusbar="$secondeBar | $statusbarPrimary"
+  ram="$(free -m | awk 'NR==2 { print $3 }') MB"
+
+  [[ ! $(du -b /tmp/chrono|awk '{print $1}') -eq 1 ]] && chrono="| $(cat /tmp/chrono)"
+
+  statusbar="${chrono} | ${battrie} | ${ram} | ${date}" 
   exec xsetroot -name "$statusbar"&
   sleep 1
 done
