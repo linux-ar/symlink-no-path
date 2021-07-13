56ca13171a26d173af6be0212a86e0e748c5658a
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2021-07-13 05:00:55 +0100

    update

--- suckless/dwm-ayoub/chrono.sh
+++ suckless/dwm-ayoub/chrono.sh
@@ -3,6 +3,12 @@ i=0
 while true;do
   let i=$i+1
   # TODO: make toop for make i-> "...0" ,"..1"
+  # TODO: make min and hour in this loop
+  #if [ $i -gt 3600 ] then;
+  #ilif 
+  # ss=i%3600
+  # mm=
+  # time="${hh}:${mm}:${ss}"
   echo  "${i}" > /tmp/chrono
   sleep 1
 done
