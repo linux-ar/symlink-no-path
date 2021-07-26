8d8da659a47228e0f394b43263eae0881ecb3c16
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2021-07-26 16:50:04 +0100

    update

--- suckless/dwm-ayoub/ayoubStatusBar.sh
+++ suckless/dwm-ayoub/ayoubStatusBar.sh
@@ -4,13 +4,14 @@ while true;do
   bateryStatus="$(cat /sys/class/power_supply/axp288_fuel_gauge/status)"
   [[ $bateryStatus =~ "Discharging" ]] && bateryFlag="off" || bateryFlag="on"
   battrie="$(cat /sys/class/power_supply/axp288_fuel_gauge/capacity)% ${bateryFlag}"
+  learn="  $(shuf -n 1 $wiki/learn.txt)  "
 
-  date="$(date +%a\ %d\ %b\ %H:%M:%S)"
+  date="$(date +%a\ %d\ %b\ %H:%M)"
   ram="$(free -m | awk 'NR==2 { print $3+$5 }') MB"
 
   [[ ! $(du -b /tmp/chrono|awk '{print $1}') -eq 1 ]] && chrono="| $(cat /tmp/chrono) " || chrono=""
 
-  statusbar="${chrono}| ${battrie} | ${ram} | ${date}" 
+  statusbar="| ${learn} ${chrono}| ${battrie} | ${ram} | ${date}" 
   exec xsetroot -name "$statusbar"&
-  sleep 1
+  sleep 10
 done
