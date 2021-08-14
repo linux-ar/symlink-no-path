2e5451ee3cd62c360c0e62f743297b182f633d06
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2021-08-14 01:24:13 +0100

    flutter: fix reload+add swapfile

--- my/tmux/init-tmux.sh
+++ my/tmux/init-tmux.sh
@@ -6,6 +6,7 @@ then
   cd /projects/flutter/ 
   flutter create $new
   flutter config --enable-linux-desktop
+  # rm -rf $new/test/
 fi
 
 echo "
@@ -13,16 +14,16 @@ echo "
 
 tmux new-session -s Flutter   -n $new -d
 tmux new-window -t Flutter -d -n nvim_config
-# tmux new-window -t Flutter -d -n debug
+tmux new-window -t Flutter -d -n debug
 tmux new-window -t Flutter -d -n terminal
 
 tmux send-keys -t Flutter:$new        \"cd /projects/flutter/$new;nvim lib/main.dart\"     Enter
 tmux send-keys -t Flutter:nvim_config \"cd $nvimdir;nvim lua/nvPlugins/packerPlugins.lua\" Enter
-# tmux send-keys -t Flutter:debug       \"cd /projects/flutter/$new;flutter run\"            Enter
+tmux send-keys -t Flutter:debug       \"cd /projects/flutter/$new;flutter run --pid-file /tmp/flutter.pid"            Enter
 tmux send-keys -t Flutter:terminal    \"cd /projects/flutter/$new\"                        Enter
 
 tmux select-window -t Flutter:$new
-tmux -u attach -t Flutter
+tmux -u switch -t Flutter
 
 " > ~/scripts/my/tmux/${new}.sh
 chmod +x ~/scripts/my/tmux/${new}.sh
