106de8312075916ce4e4cbaa9c9efa4b33575acc
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2021-10-03 04:24:52 +0100

    xdep work

---
+++ my/arch-instalation/arch-mount.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mount --types proc /proc /mnt/arch/proc
+mount --rbind /sys /mnt/arch/sys
+mount --make-rslave /mnt/arch/sys
+mount --rbind /dev /mnt/arch/dev
+mount --make-rslave /mnt/arch/dev 
+
+chroot /mnt/arch /bin/zsh 
+# source /etc/profile
+# export PS1="\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$"
