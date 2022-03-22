fe89290ef67a6a3a7e61ba821a6919703d1f95e0
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-03-22 03:08:10 +0100

    up

--- chroot/void-mount.sh
+++ chroot/void-mount.sh
@@ -3,11 +3,17 @@ isload="$(cat /tmp/isload)"
 if [[ $isload == "y" ]];then
   echo $isload
 else
-  mount --types proc /proc /sdcard/proc
-  mount --rbind /sys /sdcard/sys
-  mount --make-rslave /sdcard/sys
-  mount --rbind /dev /sdcard/dev
-  mount --make-rslave /sdcard/dev && echo "y" > /tmp/isload
+  # mount --types proc /proc /sdcard/proc
+  # mount --rbind /sys /sdcard/sys
+  # mount --make-rslave /sdcard/sys
+  # mount --rbind /dev /sdcard/dev
+  # mount --make-rslave /sdcard/dev && echo "y" > /tmp/isload
+
+  mount --rbind /sys /sdcard/sys && mount --make-rslave /sdcard/sys
+  mount --rbind /dev /sdcard/dev && mount --make-rslave /sdcard/dev
+  mount --rbind /proc /sdcard/proc && mount --make-rslave /sdcard/proc
+
+
   cat /etc/resolv.conf > /sdcard/etc/resolv.conf
 fi
 
