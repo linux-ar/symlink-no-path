8dde5d38a8cf1f977e8578ffb84dff08702c44cf
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-04-04 17:36:08 +0100

    use bettre test in chroot

--- chroot/arch-mount.sh
+++ chroot/arch-mount.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
-isload="$(cat /tmp/isload)"
-if [[ $isload == "y" ]];then
-  echo $isload
-else
-  echo "y" > /tmp/isload
-  mount --types proc /proc /mnt/arch/proc
-  mount --rbind /sys /mnt/arch/sys
-  mount --make-rslave /mnt/arch/sys
-  mount --rbind /dev /mnt/arch/dev
-  mount --make-rslave /mnt/arch/dev 
-fi
+mount --rbind /sys /mnt/arch/sys && mount --make-rslave /mnt/arch/sys
+mount --rbind /dev /mnt/arch/dev && mount --make-rslave /mnt/arch/dev
+mount --rbind /proc /mnt/arch/proc && mount --make-rslave /mnt/arch/proc
+cat /etc/resolv.conf > /mnt/arch/etc/resolv.conf
 
-chroot /mnt/arch /bin/zsh 
+chroot /mnt/arch /bin/fish
 # source /etc/profile
 # export PS1="\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$"
