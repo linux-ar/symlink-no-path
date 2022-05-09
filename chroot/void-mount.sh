678f7e5de92f9ac3cbc15cd35fa82a204248a9d1
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-05-09 19:51:55 +0100

    fix

--- chroot/void-mount.sh
+++ chroot/void-mount.sh
@@ -3,20 +3,20 @@ isload="$(cat /tmp/isload)"
 if [[ $isload == "y" ]];then
   echo $isload
 else
-  # mount --types proc /proc /sdcard/proc
-  # mount --rbind /sys /sdcard/sys
-  # mount --make-rslave /sdcard/sys
-  # mount --rbind /dev /sdcard/dev
-  # mount --make-rslave /sdcard/dev && echo "y" > /tmp/isload
+  # mount --types proc /proc /mnt/void/proc
+  # mount --rbind /sys /mnt/void/sys
+  # mount --make-rslave /mnt/void/sys
+  # mount --rbind /dev /mnt/void/dev
+  # mount --make-rslave /mnt/void/dev && echo "y" > /tmp/isload
 
-  mount --rbind /sys /sdcard/sys && mount --make-rslave /sdcard/sys
-  mount --rbind /dev /sdcard/dev && mount --make-rslave /sdcard/dev
-  mount --rbind /proc /sdcard/proc && mount --make-rslave /sdcard/proc
+  mount --rbind /sys /mnt/void/sys && mount --make-rslave /mnt/void/sys
+  mount --rbind /dev /mnt/void/dev && mount --make-rslave /mnt/void/dev
+  mount --rbind /proc /mnt/void/proc && mount --make-rslave /mnt/void/proc
 
 
-  cat /etc/resolv.conf > /sdcard/etc/resolv.conf
+  cat /etc/resolv.conf > /mnt/void/etc/resolv.conf
 fi
 
-chroot /sdcard /bin/bash 
+chroot /mnt/void /bin/bash 
 # source /etc/profile
 # export PS1="\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$"
